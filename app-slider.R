95806ed2ad7fd991a247bbfba45561c9b6552c75
chris <christopher@pop-os.localdomain>
2021-05-02 20:02:16 +0100

    dfgdfgdf

--- app-slider.R
+++ app-slider.R
@@ -17,12 +17,25 @@ url_file <- 'https://raw.github.com/gisforgringo/harvard/master/column_3C_weka.c
 
 df <-read.csv(url_file)
 
+library(tidyr)
+
+library(shinythemes)
+
+df <- df %>% drop_na()
+
 
 ####################################
 # User interface                   #
 ####################################
+print("hello ui")
 
-ui <- pageWithSidebar(
+ui <- fluidPage(theme = shinytheme("united"), 
+                
+
+  
+  
+  pageWithSidebar(
+  
   
   # Page header
   headerPanel('Orthopedic Disease Predictor'),
@@ -30,23 +43,28 @@ ui <- pageWithSidebar(
   # Input values
   sidebarPanel(
     HTML("<h3>Input parameters</h4>"),
-    sliderInput("pelvic_incidence", label = "pelvic incidence", value = 5.0,
-                min = min(df$Sepal.Length),
-                max = max(df$Sepal.Length)
+    sliderInput("pelvic_incidence", label = "pelvic incidence",  
+                value = (min(df$pelvic_incidence)+max(df$pelvic_incidence))/2,
+                min = min(df$pelvic_incidence),
+                max = max(df$pelvic_incidence)
     ),
-    sliderInput("pelvic_tilt", label = "pelvic tilt", value = 3.6,
+    sliderInput("pelvic_tilt", label = "pelvic tilt", 
+                value = (max(df$pelvic_tilt)+min(df$pelvic_tilt))/2,
                 min = min(df$pelvic_tilt),
                 max = max(df$pelvic_tilt)),
-    sliderInput("lumbar_lordosis_angle", label = "lumbar lordosis angle", value = 1.4,
+    sliderInput("lumbar_lordosis_angle", label = "lumbar lordosis angle", 
+                value = 74,
                 min = min(df$lumbar_lordosis_angle),
                 max = max(df$lumbar_lordosis_angle)),
-    sliderInput("sacral_slope", label = "sacral slope", value = 0.2,
+    sliderInput("sacral_slope", label = "sacral slope", 
+                value = 74,
                 min = min(df$sacral_slope),
                 max = max(df$sacral_slope)),
-    sliderInput("pelvic_radius", label = "pelvic radius", value = 0.2,
+    sliderInput("pelvic_radius", label = "pelvic radius", 
+                value = 120,
                 min = min(df$pelvic_radius),
-                max = max(df$pelvic_radiush)),
-    sliderInput("degree_spondylolisthesis", label = "degree spondylolisthesis", value = 0.2,
+                max = max(df$pelvic_radius)),
+    sliderInput("degree_spondylolisthesis", label = "degree spondylolisthesis", value = 200,
                 min = min(df$degree_spondylolisthesis),
                 max = max(df$degree_spondylolisthesis)),
     
@@ -59,24 +77,42 @@ ui <- pageWithSidebar(
     tableOutput('tabledata') # Prediction results table
     
   )
-)
+  
+ 
+), # end slide bar panel
+
+
+
+
+tabPanel("About", 
+         titlePanel("About"), 
+         div(includeMarkdown("project_two.html"), 
+             align="justify")
+) #tabPanel(), About
+
+
+) # end fluid page
 
 ####################################
 # Server                           #
 ####################################
 
+
+print('hello server')
 server<- function(input, output, session) {
+  
+  input
   
   # Input Data
   datasetInput <- reactive({  
     
     df <- data.frame(
-      Name = c("pelvic incidence",
-               "pelvic tilt",
-               "lumbar lordosis angle",
-               "sacral slope", 
-               "pelvic radius", 
-               "degree spondylolisthesis"),
+      Name = c("pelvic_incidence",
+               "pelvic_tilt",
+               "lumbar_lordosis_angle",
+               "sacral_slope", 
+               "pelvic_radius", 
+               "degree_spondylolisthesis"),
       Value = as.character(c(input$pelvic_incidence,
                              input$pelvic_tilt,
                              input$lumbar_lordosis_angle,
@@ -85,12 +121,20 @@ server<- function(input, output, session) {
                              input$degree_spondylolisthesis)),
       stringsAsFactors = FALSE)
     
+    df
+    
     Species <- 0
     df <- rbind(df, Species)
     input <- transpose(df)
     write.table(input,"input.csv", sep=",", quote = FALSE, row.names = FALSE, col.names = FALSE)
     
     test <- read.csv(paste("input", ".csv", sep=""), header = TRUE)
+    
+    print("test")
+    print(test)
+    print('end test')
+    
+   
     
     Output <- data.frame(Prediction=predict(model,test), round(predict(model,test,type="prob"), 3))
     print(Output)
